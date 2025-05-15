4901c0cb8de036ec584afacd5a5d3336bc4f83a8
自分の姓名（ローマ字） <takuma.ride@gmail.com>
2025-05-15 22:56:27 +0900

    プロトタイプ関連実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,14 +1,59 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index]
+before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+before_action :move_to_index, only: [:edit,]
   def index
     @users = User.all
-    # 一覧ページの処理（誰でも見られる）
+    @prototypes = Prototype.all
+  end
+  
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
   end
 
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+ 
+  end
+  def new
+    @prototype = Prototype.new
+    # 新規作成ページの処理（ログインユーザーのみ）
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+    
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+
   private
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 
   def move_to_index
-    unless prototype.user_id == current_user.id
+    unless @prototype.user_id == current_user.id
       redirect_to action: :index
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'prototypes#index'        # その他のルート
-  resources :users, only: [:show,:index]
+  root 'prototypes#index'
+  resources :users, only: [:index, :new, :create, :destroy, :edit]
   resources :prototypes
 end
+
+#resources の役割
+#resources は RESTfulなURL（ルーティング）と、
+#それに対応するコントローラのアクションをまとめて定義する仕組み
+
+#root の役割
+#root は アプリのトップページのURL（"/"）をどこに割り当てるかを決めるもの
+
+#devise_for :users の役割
+#devise は、Ruby on Railsの中で
+#ユーザー認証（ログイン・ログアウト・登録など）を簡単に実装できる仕組み
+#これをルーティングに書くと、以下のようなURLとアクションが自動で作られます：
+
+#URL	         HTTPメソッド	      用途
+#/users/sign_in 	   GET	   ログインフォーム表示
+#/users/sign_in 	   POST	     ログイン処理
+#/users/sign_out	  DELETE	   ログアウト処理
+#/users/sign_up 	    GET	    新規登録フォーム表示
+#/users	              POST	   新規登録処理
+#/users/password/new	GET	   パスワード再設定フォーム表示
